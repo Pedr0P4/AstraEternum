0c767f705ed5d13319cf1f735a45d2fedd6cf3ec
Pedro Paulo <pedropaulo062011@gmail.com>
2025-05-15 14:59:05 -0300

    Modificação do spritesheet do player e adição de script (movimentação e animação)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+const VELOCITY: int = 250;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	
+	var mouse_pos = get_viewport().get_mouse_position();
+	
+	if velocity != Vector2.ZERO:
+		if mouse_pos.y > position.y:
+			$PlayerSprites.play("WalkingDown");
+		else:
+			$PlayerSprites.play("WalkingUp");
+	else:
+		if mouse_pos.y > position.y:
+			$PlayerSprites.play("IdleF");
+		else:
+			$PlayerSprites.play("IdleB");
+			
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down");
+	velocity = direction * VELOCITY;
+	
+	move_and_slide();
