189431ae38ec246f29f30906e26b3e88bfb3f3ec
Pedro Paulo <pedropaulo062011@gmail.com>
2025-05-20 07:54:42 -0300

    Resolução do bug dos braços do player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -12,22 +12,25 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	var mouse_pos = get_viewport().get_mouse_position();
-	var globalM_pos = get_global_mouse_position();
+	var mouse_pos: Vector2 = get_viewport().get_mouse_position();
+	var globalM_pos: Vector2 = get_global_mouse_position();
+	var x_cap: float = abs(globalM_pos.y - position.y)*1.2;
 	
 	if velocity != Vector2.ZERO:
 		if mouse_pos.y > position.y:
-			player_sprite.play("WalkingDown");
+			player_sprite.play("Move_down");
+		elif (mouse_pos.x < position.x + x_cap && mouse_pos.x > position.x - x_cap):
+			player_sprite.play("Move_up");
 		else:
-			player_sprite.play("WalkingUp");
+			player_sprite.play("Move_down");
 	else:
-		if mouse_pos.y <= position.y - 100 && (mouse_pos.x < position.x + 150 && mouse_pos.x > position.x - 150):
+		if mouse_pos.y <= position.y && (mouse_pos.x < position.x + x_cap && mouse_pos.x > position.x - x_cap):
 			player_sprite.play("IdleB");
 		else:
 			player_sprite.play("IdleF");
 	
 	$Arms.z_index = 1;
-	if(globalM_pos.x < position.x + 150 && globalM_pos.x > position.x - 150):
+	if(globalM_pos.x < position.x + x_cap && globalM_pos.x > position.x - x_cap):
 		$Arms.play("Up_Down");
 		$Arms.rotate(-(PI/2));
 		$Arms.flip_v = false;
